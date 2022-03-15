7d5e2d3a77135bbfb69c6c3369f525356a8241c5
Dan <dan@dminc.ru>
2022-03-15 21:10:07 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Problem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Problem
+    {
+        public int FirstElement { get; set; }
+        public int SecondElement { get; set; }
+        public string Action { get; set; }
+        public bool flag = false;
+        public Problem(int a, int b, string action)
+        {
+            FirstElement = a;
+            SecondElement = b;
+            Action = action;
+        }
+
+        public int Resolve()
+        {
+            switch (Action)
+            {
+                case "+": return FirstElement + SecondElement;
+                case "-": return FirstElement - SecondElement;
+                case "/": return FirstElement / SecondElement;
+                case "*": return FirstElement * SecondElement;
+                default:
+                    {
+                        flag = true;
+                        return -1;
+                    }
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine("Введите первое число.");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число.");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Какое действие нужно выполнить? (+ - / *)");
+                string action = Console.ReadLine();
+                Problem expression = new Problem(a, b, action);
+
+                int answer = expression.Resolve();
+                if (expression.flag)
+                {
+                    Console.WriteLine("Неверный ввод.");
+                } else
+                {
+                    Console.WriteLine(answer);
+                }
+
+                Console.WriteLine("Завершить работу? (y/n)");
+                string userAnswer = Console.ReadLine();
+                if (userAnswer == "y")
+                    break;
+            }
+        }
+    }
+}
